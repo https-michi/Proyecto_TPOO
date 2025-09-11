8ef76b2514ae416cf0f9442f629dc75ecff2fa83
https-michi <glitchmichi@gmail.com>
2025-09-11 17:41:37 -0500

    chore(init): configuración inicial del proyecto

---
+++ CarsAccessoriesApp/src/main/java/com/pro/carsaccessoriesapp/CarsAccessoriesApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pro.carsaccessoriesapp;
+
+/**
+ *
+ * @author snowden
+ */
+public class CarsAccessoriesApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
